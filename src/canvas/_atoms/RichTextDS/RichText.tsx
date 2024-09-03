aaf6ef767e8e8318843bda63bec155b8f1197039
Michal Majer <Michal.Majer@StoneX.com>
2024-09-03 17:11:59 +0200

    Lint

--- src/canvas/_atoms/RichTextDS/RichText.tsx
+++ src/canvas/_atoms/RichTextDS/RichText.tsx
@@ -1,15 +1,17 @@
 import { FC } from 'react';
 import { RichTextProps } from '.';
 import { nodeParser } from '@kontent-ai/delivery-node-parser';
-import { createDeliveryClient, createRichTextHtmlResolver } from '@kontent-ai/delivery-sdk';
+import { createDeliveryClient, createRichTextHtmlResolver, Elements, IContentItem } from '@kontent-ai/delivery-sdk';
 
 const deliveryClient = createDeliveryClient({
   environmentId: process.env.KONTENT_AI_ENVIRONMENT_ID || '',
 });
+type Content = IContentItem<{
+  content: Elements.RichTextElement;
+}>;
 
 export const RichText: FC<RichTextProps> = async ({ text }) => {
-  // @typescript-eslint/no-explicit-any
-  const response = await deliveryClient.item<any>('ai_robotic_powered_vehicle_kit_25a0265').toPromise();
+  const response = await deliveryClient.item<Content>('ai_robotic_powered_vehicle_kit_25a0265').toPromise();
   const richTextElement = response.data.item.elements.content;
 
   console.log('richTextElement.images', richTextElement.images);
